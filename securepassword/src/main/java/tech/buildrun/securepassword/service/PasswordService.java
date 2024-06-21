22931c9118bd8abc46e3a135b0f666ff94e175a2
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-06-20 21:05:16 -0300

    :sparkles: `feat` Add validations password

--- securepassword/src/main/java/tech/buildrun/securepassword/service/PasswordService.java
+++ securepassword/src/main/java/tech/buildrun/securepassword/service/PasswordService.java
@@ -1,9 +1,12 @@
 package tech.buildrun.securepassword.service;
 
+import io.micrometer.common.util.StringUtils;
 import org.springframework.stereotype.Service;
+import tech.buildrun.securepassword.utils.Messages;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class PasswordService {
@@ -11,7 +14,42 @@ public class PasswordService {
     public List<String> validatePass(String pass) {
         List<String> failures = new ArrayList<>();
 
+        validateLength(pass, failures);
+        validateUppercase(pass, failures);
+        validateLowercase(pass, failures);
+        validateNumber(pass, failures);
+        validateSpecialChars(pass, failures);
+
         return failures;
     }
 
+    private static void validateLength(String pass, List<String> failures) {
+        if (StringUtils.isBlank(pass) || pass.length() < 8) {
+            failures.add(Messages.PASSWORD_MIN_LENGTH);
+        }
+    }
+
+    private static void validateUppercase(String pass, List<String> failures) {
+        if (!Pattern.matches(".*[A-Z].*", pass)) {
+            failures.add(Messages.PASSWORD_UPPERCASE);
+        }
+    }
+
+    private static void validateLowercase(String pass, List<String> failures) {
+        if (!Pattern.matches(".*[a-z].*", pass)) {
+            failures.add(Messages.PASSWORD_LOWERCASE);
+        }
+    }
+
+    private static void validateNumber(String pass, List<String> failures) {
+        if (!Pattern.matches(".*[0-9].*", pass)) {
+            failures.add(Messages.PASSWORD_NUMBER);
+        }
+    }
+
+    private static void validateSpecialChars(String pass, List<String> failures) {
+        if (!Pattern.matches(".*[\\W].*", pass)) {
+            failures.add(Messages.PASSWORD_SPECIAL_CHAR);
+        }
+    }
 }
---
+++ securepassword/src/main/java/tech/buildrun/securepassword/utils/Messages.java
@@ -0,0 +1,9 @@
+package tech.buildrun.securepassword.utils;
+
+public class Messages {
+    public static final String PASSWORD_MIN_LENGTH = "A senha deve possuir pelo menos 08 caracteres.";
+    public static final String PASSWORD_UPPERCASE = "A senha deve possuir pelo menos uma letra maiúscula.";
+    public static final String PASSWORD_LOWERCASE = "A senha deve possuir pelo menos uma letra minúscula.";
+    public static final String PASSWORD_NUMBER = "A senha deve possuir pelo menos um numero.";
+    public static final String PASSWORD_SPECIAL_CHAR = "A senha deve possuir pelo menos um caractere especial.";
+}
